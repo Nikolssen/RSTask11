6b11f9eb7514857f57e3181a18bee8379d3738df
Ivan Budovich <ibudovichivan3@gmail.com>
2021-09-08 10:39:31 +0300

    Add Fonts

--- RSTask11/Resourses/Assets.swift
+++ RSTask11/Resourses/Assets.swift
@@ -68,3 +68,24 @@ extension UIImage {
         return UIImage(named: "hyperlink")!
     }
 }
+
+extension UIFont {
+    
+    static var robotoBold14: UIFont {
+        return UIFont(name: "Roboto-Bold", size: 14)!
+    }
+    
+    static var robotoBold24: UIFont {
+        return UIFont(name: "Roboto-Bold", size: 24)!
+    }
+    
+    static var robotoBold48: UIFont {
+        return UIFont(name: "Roboto-Bold", size: 48)!
+    }
+    
+    static var robotoMedium17: UIFont {
+        return UIFont(name: "Roboto-Medium", size: 17)!
+    }
+    
+    
+}
