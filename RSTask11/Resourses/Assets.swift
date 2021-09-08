965a7d91c9913790c78912cb41586dcab9333432
Ivan Budovich <ibudovichivan3@gmail.com>
2021-09-08 09:21:08 +0300

    Add assets

---
+++ RSTask11/Resourses/Assets.swift
@@ -0,0 +1,70 @@
+//
+//  Assets.swift
+//  RSTask11
+//
+//  Created by Ivan Budovich on 9/8/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    static var smockyBlack: UIColor {
+        return UIColor(named: "Smocky Black")!
+    }
+    
+    static var superWhite: UIColor {
+        return UIColor(named: "Super White")!
+    }
+    
+    static var champagne: UIColor {
+        return UIColor(named: "Champagne")!
+    }
+    
+    static var slateGray: UIColor {
+        return UIColor(named: "Slate Gray")!
+    }
+    
+    static var glaucous: UIColor {
+        return UIColor(named: "Glaucous")!
+    }
+    
+    static var queenBlue: UIColor {
+        return UIColor(named: "Queen Blue")!
+    }
+    
+    static var coral: UIColor {
+        return UIColor(named: "Coral")!
+    }
+    
+    static var cyanProcess: UIColor {
+        return UIColor(named: "Cyan Process")!
+    }
+    
+}
+
+extension UIImage {
+    static var rocket: UIImage {
+        return UIImage(named: "rocket")!
+    }
+    
+    static var adjustment: UIImage {
+        return UIImage(named: "adjustment")!
+    }
+    
+    static var lego: UIImage {
+        return UIImage(named: "lego")!
+    }
+    
+    static var bidirectionalArrow: UIImage {
+        return UIImage(named: "biarrow")!
+    }
+    
+    static var checkmark: UIImage {
+        return UIImage(named: "checkmark")!
+    }
+    
+    static var hyperlink: UIImage {
+        return UIImage(named: "hyperlink")!
+    }
+}
