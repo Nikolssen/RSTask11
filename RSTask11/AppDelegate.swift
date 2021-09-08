d988b6aa04bf553d87cb0730e380c38a2686463f
Ivan Budovich <ibudovichivan3@gmail.com>
2021-09-08 08:44:16 +0300

    Init Project

---
+++ RSTask11/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RSTask11
+//
+//  Created by Ivan Budovich on 9/8/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+        return true
+    }
+
+
+}
+
